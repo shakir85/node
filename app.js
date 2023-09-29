38872fc5e7d3ffc9cd244b60bb6a833ad4a3d978
shakir <sralmasaari@gmail.com>
2023-09-28 20:31:14 -0700

    Add post

--- app.js
+++ app.js
@@ -1,33 +1,72 @@
 const express = require("express");
 const app = express();
+const bodyPasser = require("body-parser");
 
-const message = [
-  {
-    id: 1,
-    name: "Joe Doe",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    read: false,
-  },
-  {
-    id: 2,
-    name: "Nol Ryo",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    read: false,
-  },
-  {
-    id: 1,
-    name: "Pol Dee",
-    content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    read: true,
-  },
-];
+let messages = [];
+let id = 1;
+
+app.use(bodyPasser.json());
 
 app.get("/", function (req, res) {
   res.send("Messenger System");
 });
 
+// GET localhost:3000/messages
 app.get("/messages", function (req, res) {
-  res.json(message);
+  res.json(messages);
+});
+
+// GET localhost:3000/messages
+app.get("/messages/:id", function (req, res) {
+  var id = parseInt(req.params.id, 10);
+  var flag = false;
+
+  for (var i = 0; i < messages.length; i++) {
+    if (messages[i].id === id) {
+      res.json(messages[i]);
+      flag = true;
+      break;
+    }
+  }
+
+  if (!flag) {
+    res.json({ msg: `Cannot find a message with id: ${id}` });
+  }
+});
+
+// POST localhost:3000/messages
+app.post("/messages/", function (req, res) {
+  let body = req.body;
+
+  let new_message = {
+    id: id++,
+    name: body.name,
+    content: body.content,
+    read: body.read,
+  };
+
+  messages.push(new_message);
+  res.json({ msg: "New message added" });
+});
+
+// DELETE localhost:3000/messages
+app.delete("/messages/:id", function (req, res) {
+  let id = parseInt(req.params.id, 10);
+  let flag = false;
+
+  for (let i = 0; i < messages.length; i++) {
+    if (messages[i].id === id) {
+      messages.splice(i, 1);
+      flag = true;
+      break;
+    }
+  }
+
+  if (!flag) {
+    res.json({ msg: `Cannot find a message with id: ${id}` });
+  } else {
+    res.json({ msg: `Message id: ${id} has been deleted` });
+  }
 });
 
 app.listen(3000, function () {
